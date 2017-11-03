ddfc9fe5c1efbe7d1a625a67a6e730ed46619348
dmall <wanli.zhang@dmall.com>
2017-11-04 05:28:19 +0800

    add learning-multiple

---
+++ multiple-domain/src/main/java/zwl/learning/multiple/App.java
@@ -0,0 +1,13 @@
+package zwl.learning.multiple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
